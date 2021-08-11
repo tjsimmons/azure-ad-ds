00d3b3cb3060029e4ebdae10e79180990ed56007
T.J. Simmons <tj@tjsimmons.net>
2021-08-11 05:40:04 -0500

    Set up Bastion

--- main.tf
+++ main.tf
@@ -38,8 +38,28 @@ resource "azurerm_subnet" "azure_ad_ds_workloads_subnet" {
 }
 
 resource "azurerm_subnet" "azure_ad_ds_bastion_subnet" {
-  name = "AzureBastionSubnet"
+  name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.azure_ad_ds_rg.name
   virtual_network_name = azurerm_virtual_network.azure_ad_ds_vnet.name
   address_prefixes     = ["10.3.0.0/24"]
+}
+
+resource "azurerm_public_ip" "azure_ad_ds_pip" {
+  name                = "azure-ad-ds-pip"
+  resource_group_name = azurerm_resource_group.azure_ad_ds_rg.name
+  location            = azurerm_resource_group.azure_ad_ds_rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "azure_ad_ds_bastion" {
+  name                = "azure-ad-ds-bastion"
+  resource_group_name = azurerm_resource_group.azure_ad_ds_rg.name
+  location            = azurerm_resource_group.azure_ad_ds_rg.location
+
+  ip_configuration {
+    name                 = "azure-ad-ds-bastion-ipc"
+    subnet_id            = azurerm_subnet.azure_ad_ds_bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.azure_ad_ds_pip.id
+  }
 }
